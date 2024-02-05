b399da93067625e4e1a22598b7a3d26052f2ebd0
benghalem <benghalemaziz@gmail.com>
2024-02-05 11:48:30 +0100

    Page + routing and templet

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+import styles from "./styles.module.css"
+
+const { footerContainer } = styles
+const Footer = () => {
+  return (
+    <div className={footerContainer}> Copyright © 2022 Our Ecom. All rights reserverd. </div>
+  )
+}
+
+export default Footer
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-
 import ReactDOM from 'react-dom/client'
+import AppRouter from '@routes/AppRouter'
+
+
+// Styles
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@styles/global.css";
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <div>
-    hello
-  </div>
+  <AppRouter/>
 )
